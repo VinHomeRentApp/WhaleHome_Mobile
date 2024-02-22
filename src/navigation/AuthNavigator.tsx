50ea9bd01129f1f78fee2b1d524f5dd38f01ec12
tranquangminh0110 <bibabibum0110@gmail.com>
2024-02-22 16:24:47 +0700

    update authnavigator

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,22 +1,4 @@
 // AuthNavigator.tsx
-<<<<<<< HEAD
-import { NativeStackScreenProps, createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from '@screens/Authentication/LoginScreen';
-import OnboardingScreen from '@screens/Authentication/OnboardingScreen';
-import SignUpScreen from '@screens/Authentication/SignUpScreen/SignUpScreen';
-import React from 'react';
-
-export type StackParamList = {
-  LoginScreen: undefined;
-  OnboardingScreen: undefined;
-  SignUpScreen: undefined;
-};
-export type OnBoardingProps = NativeStackScreenProps<StackParamList, 'OnboardingScreen'>;
-export type LoginScreenProps = NativeStackScreenProps<StackParamList, 'LoginScreen'>;
-export type SignUpScreenProps = NativeStackScreenProps<StackParamList, 'SignUpScreen'>;
-
-const Stack = createNativeStackNavigator<StackParamList>();
-=======
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '@screens/Authentication/LoginScreen';
 import OnboardingScreen from '@screens/Authentication/OnboardingScreen';
@@ -25,7 +7,6 @@ import { AuthStackParamList } from '@type/navigation.types';
 import React from 'react';
 
 const Stack = createNativeStackNavigator<AuthStackParamList>();
->>>>>>> b0576ff2010405596ce1be901f73e35197a2f26c
 
 const AuthNavigator = () => {
   return (

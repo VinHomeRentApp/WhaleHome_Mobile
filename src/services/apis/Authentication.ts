9370d05983cdbc97252308cc4d74ecd45e7f3f13
Nguyễn Trung Kiên <93828844+Trungkien03@users.noreply.github.com>
2024-02-23 18:40:53 +0700

    modify services

--- src/services/apis/Authentication.ts
+++ src/services/apis/Authentication.ts
@@ -1,33 +1,7 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ApiResponse } from '@type/response.types';
-import axios from 'axios';
-import { Alert } from 'react-native';
+import axios, { AxiosResponse } from 'axios';
 
-class Authentication {
-  public async signIn(email: string, password: string): Promise<string> {
-    try {
-      const response = await axios.post<ApiResponse<{ access_token: string }>>(
-        'https://whalehome.up.railway.app/api/v1/auth/authenticate',
-        {
-          email,
-          password
-        }
-      );
-      if (response && response.data) {
-        const idToken = response.data.data.access_token;
-        // Store the ID token in AsyncStorage
-        await AsyncStorage.setItem('authToken', idToken);
-        return idToken; // Return the token for any further client-side use
-      } else {
-        throw new Error('Empty response data');
-      }
-    } catch (error: any) {
-      Alert.alert('SignIn Error', error.message);
-      throw error;
-    }
-  }
+const SIGN_IN_URL = 'https://whalehome.up.railway.app/api/v1/auth/authenticate';
+
+export async function signIn(email: string, password: string): Promise<AxiosResponse<{ access_token: string }>> {
+  return axios.post<{ access_token: string }>(SIGN_IN_URL, { email, password });
 }
-
-const authentication = new Authentication();
-
-export default authentication;
--- src/types/response.types.ts
+++ src/types/response.types.ts
@@ -2,8 +2,3 @@ export interface SuccessfullResponse<TData> {
   message: string;
   object: TData;
 }
-
-export interface ApiResponse<T> {
-  message: string;
-  data: T;
-}

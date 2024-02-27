920790779f650819811d2a8bd198584828516c9c
tranquangminh0110 <116641540+tranquangminh0110@users.noreply.github.com>
2024-02-27 11:07:34 +0700

    Work/feature dev/modify login screen (#20)
    * modify continue button
    
    * 70% SignupScreen
    
    * updae
    
    * update authnavigator
    
    * update api services register
    
    * minh vua xoa yarn nha
    
    * profile screen

--- src/screens/Home/EditProfileScreen.tsx
+++ src/screens/Home/EditProfileScreen.tsx
@@ -1,24 +1,32 @@
-import { View, StyleSheet, Image, TextInput, Button, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import React, { useState } from 'react';
 import globalStyle from '@styles/globalStyle';
-import { Call, GalleryAdd, Message, Profile } from 'iconsax-react-native';
+import { Call, GalleryAdd, Message, Profile, Blend, Home3 } from 'iconsax-react-native';
 import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { backgroundColor, typoColor } from '@constants/appColors';
 import * as ImagePicker from 'expo-image-picker';
+import { EditProfileScreenProps } from '@type/navigation.types';
+import TextComponent from '@components/ui/TextComponent';
 
 interface FormData {
   username: string;
   phoneNumber: string;
   email: string;
+  gender: string;
+  address: string;
 }
+const IMAGE_DEFAULT = '../../assets/images/user/kien.jpg';
 
-const EditProfileScreen = () => {
+const EditProfileScreen = ({ route }: EditProfileScreenProps) => {
   const [image, setImage] = useState<string>();
+  const profileEditInfo = route.params.userEditProfile;
   const { control, handleSubmit } = useForm<FormData>({
     defaultValues: {
-      email: 'Mathew@email.com',
-      phoneNumber: '+62 112-3288-9111',
-      username: 'Mathew Adam'
+      email: profileEditInfo.email,
+      phoneNumber: profileEditInfo.phone,
+      username: profileEditInfo.fullName,
+      gender: profileEditInfo.gender,
+      address: profileEditInfo.address
     }
   });
 
@@ -43,46 +51,69 @@ const EditProfileScreen = () => {
   };
 
   return (
-    <View style={[globalStyle.container, { alignItems: 'center' }]}>
-      <View style={[styles.profileInfo]}>
-        <Image style={[styles.profileImage]} source={{ uri: image }} />
-        <TouchableOpacity style={[styles.editButton]} onPress={pickImage}>
-          <GalleryAdd size='20' color={backgroundColor.black1} />
+    <ScrollView style={[globalStyle.container]}>
+      <View style={[styles.fullScreen]}>
+        <View style={[styles.profileInfo]}>
+          <Image style={[styles.profileImage]} source={{ uri: route.params.userEditProfile.image || IMAGE_DEFAULT }} />
+          <TouchableOpacity style={[styles.editButton]} onPress={pickImage}>
+            <GalleryAdd size='20' color={backgroundColor.black1} />
+          </TouchableOpacity>
+        </View>
+        <Controller
+          control={control}
+          name='username'
+          render={({ field: { value, onChange } }) => (
+            <View style={[styles.inputContainer]}>
+              <TextInput value={value} onChangeText={onChange} style={[styles.inputField]} />
+              <Profile size='25' color='#252B5C' />
+            </View>
+          )}
+        />
+        <Controller
+          control={control}
+          name='phoneNumber'
+          render={({ field: { value, onChange } }) => (
+            <View style={[styles.inputContainer]}>
+              <TextInput value={value} onChangeText={onChange} style={[styles.inputField]} />
+              <Call size='25' color='#252B5C' />
+            </View>
+          )}
+        />
+        <Controller
+          control={control}
+          name='email'
+          render={({ field: { value, onChange } }) => (
+            <View style={[styles.inputContainer]}>
+              <TextInput value={value} onChangeText={onChange} style={[styles.inputField]} />
+              <Message size='25' color='#252B5C' />
+            </View>
+          )}
+        />
+        <Controller
+          control={control}
+          name='gender'
+          render={({ field: { value, onChange } }) => (
+            <View style={[styles.inputContainer]}>
+              <TextInput value={value} onChangeText={onChange} style={[styles.inputField]} />
+              <Blend size='25' color='#252B5C' />
+            </View>
+          )}
+        />
+        <Controller
+          control={control}
+          name='address'
+          render={({ field: { value, onChange } }) => (
+            <View style={[styles.inputContainer]}>
+              <TextInput value={value} onChangeText={onChange} style={[styles.inputField]} />
+              <Home3 size='25' color='#252B5C' />
+            </View>
+          )}
+        />
+        <TouchableOpacity onPress={handleSubmit(onSubmit)} style={[styles.buttonSubmit]}>
+          <TextComponent content='Submit' textColor={typoColor.black1} />
         </TouchableOpacity>
       </View>
-      <Controller
-        control={control}
-        name='username'
-        render={({ field: { value, onChange } }) => (
-          <View style={[styles.inputContainer]}>
-            <TextInput value={value} onChangeText={onChange} style={[styles.inputField]} placeholder='123' />
-            <Profile size='25' color='#252B5C' />
-          </View>
-        )}
-      />
-      <Controller
-        control={control}
-        name='phoneNumber'
-        render={({ field: { value, onChange } }) => (
-          <View style={[styles.inputContainer]}>
-            <TextInput value={value} onChangeText={onChange} style={[styles.inputField]} placeholder='123' />
-            <Call size='25' color='#252B5C' />
-          </View>
-        )}
-      />
-      <Controller
-        control={control}
-        name='email'
-        render={({ field: { value, onChange } }) => (
-          <View style={[styles.inputContainer]}>
-            <TextInput value={value} onChangeText={onChange} style={[styles.inputField]} placeholder='123' />
-            <Message size='25' color='#252B5C' />
-          </View>
-        )}
-      />
-
-      <Button title='Submit' onPress={handleSubmit(onSubmit)} />
-    </View>
+    </ScrollView>
   );
 };
 
@@ -123,6 +154,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 100
+  },
+  fullScreen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonSubmit: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginTop: 20,
+    borderRadius: 12,
+    backgroundColor: typoColor.yellow1
   }
 });
 

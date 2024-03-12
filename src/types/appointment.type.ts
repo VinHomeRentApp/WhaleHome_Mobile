f0352f698d8b7b1e9cf34191b71bcb8caa6ee69a
tranquangminh0110 <116641540+tranquangminh0110@users.noreply.github.com>
2024-03-12 18:15:17 +0700

    Work/feature appointmentscreen (#48)
    * update appointment screen
    
    * create Appointment section and appointment card

---
+++ src/types/appointment.type.ts
@@ -0,0 +1,65 @@
+type Area = {
+  id: number;
+  createDate: string;
+  status: boolean;
+  name: string;
+};
+
+type Zone = {
+  id: number;
+  createDate: string;
+  status: boolean;
+  name: string;
+  area: Area;
+};
+
+type Building = {
+  id: number;
+  createDate: string;
+  status: boolean;
+  name: string;
+  zone: Zone;
+};
+
+type ApartmentClass = {
+  id: number;
+  createDate: null;
+  status: boolean;
+  name: string;
+  rent_price: number;
+  buy_price: number;
+  width: number;
+  length: number;
+  height: number;
+};
+
+type Apartment = {
+  id: number;
+  createDate: null;
+  status: boolean;
+  name: string;
+  description: string;
+  living_room: number;
+  bed_room: number;
+  kitchen: number;
+  rest_room: number;
+  floor: number;
+  area: number;
+  air_conditioner: number;
+  electric_fan: number;
+  television: number;
+  electric_stoves: number;
+  gas_stoves: number;
+  apartmentClass: ApartmentClass;
+  building: Building;
+};
+
+export type Appointment = {
+  statusAppointment: string;
+  dateTime: string;
+  time: null;
+  createBy: null;
+  apartment: Apartment;
+  note: string;
+  address: string;
+};

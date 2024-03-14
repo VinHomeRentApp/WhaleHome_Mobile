235f6cc7795b0851303d3237e81eaf8ba75f96f8
Nguyễn Trung Kiên <93828844+Trungkien03@users.noreply.github.com>
2024-03-14 15:40:46 +0700

    fix image url

--- src/models/class/PostImage.class.ts
+++ src/models/class/PostImage.class.ts
@@ -1,10 +1,10 @@
 class PostImage {
   id: number;
   createDate: string | Date;
-  image_url: string | null;
+  image_url: string;
   image_alt: string;
 
-  constructor(id: number, createDate: string | Date, image_url: string | null, image_alt: string) {
+  constructor(id: number, createDate: string | Date, image_url: string, image_alt: string) {
     this.id = id;
     this.createDate = createDate;
     this.image_url = image_url;
